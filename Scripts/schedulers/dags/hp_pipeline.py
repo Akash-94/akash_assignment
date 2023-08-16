16d3d416ff66a8c77cb7ea99b5c7d737e22019ce
Akash-94 <56532748+Akash-94@users.noreply.github.com>
2023-08-16 15:47:43 +0530

    Update hp_pipeline.py

--- Scripts/schedulers/dags/hp_pipeline.py
+++ Scripts/schedulers/dags/hp_pipeline.py
@@ -2,10 +2,7 @@ from os import path
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-import logging
 
-
-SCRAPER_PATH = path.abspath(path.join(path.dirname(__file__), '../../scrapers'))
 
 DEFAULT_ARGS = {
     'owner': 'airflow',
@@ -21,16 +18,17 @@ with DAG('hp_pipeline',
 
     SCRAPING_TASK = BashOperator(
         task_id = 'scrape',
-        bash_command = cd "path.abspath(path.join(path.dirname(__file__), '../../scrapers')) && scrapy crawl hp_treasury"
+        bash_command = 'cd /home/akash/airflow/akash_assignment-main/Scripts/scrapers/scrapers/spiders && scrapy crawl hp_treasury'
     )
     PREPROCESSING_TASK = BashOperator(
         task_id = 'preprocessing',
-        bash_command = cd "path.abspath(path.join(path.dirname(__file__), '../schedulers/dags')) && python preprocessing.py"
+        bash_command = 'cd /home/akash/airflow/akash_assignment-main/Scripts && python3 preprocessing.py'
     )
     WRITE_DB = BashOperator(
         task_id = 'write_db',
-        bash_command = cd "path.abspath(path.join(path.dirname(__file__), '../schedulers/dags')) && python write_db.py"
+        bash_command = 'cd /home/akash/airflow/akash_assignment-main/Scripts && python3 write_db.py'
     )
 
 SCRAPING_TASK.set_downstream(PREPROCESSING_TASK)
 PREPROCESSING_TASK.set_downstream(WRITE_DB)
+
